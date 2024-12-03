0db479fdc713b5c0da2728ca04f0ab5f94ac7a78
Zaidh Imran <zaidhimran2000@gmail.com>
2024-12-03 16:10:20 +0200

    npm update

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\ASUS\My project\nextjs-starter-onceui\src\app\layout.tsx
+// File: /Users/zaidh/Projects/FillUsIn/freegazans/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
